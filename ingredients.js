9814fb36c289157c15b6d948e9a0102fbbc895b7
Kyrylo Kharchenko <ikyden23@gmail.com>
2022-06-24 05:05:49 +0000

    js file with three array loops

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+// Write a for loop that prints out the contents of ingredients:
+for (let y = 0; y < ingredients.length;y++) {
+  console.log(ingredients[y]);
+} 
+// Write any loop (while or for) that prints out the contents of ingredients backwards
+for (let z = 7; z >= 0; z--) {
+  console.log(ingredients[z]);
+}
